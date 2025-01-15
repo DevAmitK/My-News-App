73f86c0cc7cad12460193bca6d21fac6b0fa8a4a
Amit <amitkundu.developer@gmail.com>
2025-01-15 12:37:25 +0530

    init

---
+++ app/src/main/java/com/example/mynewsapp/data/ApiCall/APIService.kt
@@ -0,0 +1,15 @@
+package com.example.mynewsapp.data.ApiCall
+
+import com.example.mynewsapp.data.model.NewsModel
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface APIService {
+
+    @GET("top-headlines")
+    suspend fun getAllNews(
+        @Query("country") country: String = "us",
+        @Query("apiKey") apiKey: String = "a98b0bbe8caa4bcca2c845fd70aa9d58"
+    ): NewsModel
+
+}
---
+++ app/src/main/java/com/example/mynewsapp/data/ApiCall/ApiBuilder.kt
@@ -0,0 +1,19 @@
+package com.example.mynewsapp.data.ApiCall
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiBuilder {
+
+
+    fun provideApi() =
+        Retrofit.Builder()
+            .baseUrl("https://newsapi.org/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(APIService::class.java)
+
+
+}
